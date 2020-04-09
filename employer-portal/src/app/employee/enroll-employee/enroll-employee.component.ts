97654c9525cb4f8444fcf7a1fed284a025a6bc27
Prafull Kulshrestha <prafullkulshrestha@ALIPL0857.local>
2020-04-09 11:29:59 +0530

    Completed the enroll employee functionality

--- employer-portal/src/app/employee/enroll-employee/enroll-employee.component.ts
+++ employer-portal/src/app/employee/enroll-employee/enroll-employee.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployerService } from '../../shared/services/employee.service';
 import { Employee } from '../../models/Employee';
+import {FormControl, Validators, FormGroup} from '@angular/forms';
+import { Department } from '../../models/Department';
+import * as moment from 'moment';
+import {MatSnackBar, MatSnackBarConfig,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition} from '@angular/material';
+import { error } from '../../../../node_modules/protractor';
+
 @Component({
   selector: 'app-enroll-employee',
   templateUrl: './enroll-employee.component.html',
@@ -8,24 +16,53 @@ import { Employee } from '../../models/Employee';
 })
 export class EnrollEmployeeComponent implements OnInit {
 
-   employee: Employee = new Employee();
-   submitted = false;
+   employee: any;
+   departments: Department [];
+   horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+   verticalPosition: MatSnackBarVerticalPosition = 'bottom';
+   public controls: { [propName: string]: FormControl } = {
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    gender: new FormControl(''),
+    dob: new FormControl(''),
+    department: new FormControl('')
 
-  constructor(private employeeService: EmployerService) { }
+  };
+
+   public employeeFormGroup: FormGroup;
+  constructor(private employerService: EmployerService, private snackBar: MatSnackBar) { }
 
   ngOnInit() {
+    this.employeeFormGroup = new FormGroup (this.controls);
+    this.employerService.getDepartmentList().subscribe((departments)=> this.departments = departments,
+  (error) => {console.error('Error')})
   }
 
   save() {
-    this.employeeService.createEmployee(this.employee)
+    let dob: string = moment(this.controls.dob.value).format('YYYY-MM-DD');
+    let employee: any = {
+      firstName: this.controls.firstName.value,
+      lastName: this.controls.lastName.value,
+      gender: this.controls.gender.value,
+      dateOfBirth: dob ,
+      department: {
+        departmentId: this.controls.department.value
+      }
+    }
+    this.employerService.createEmployee(employee)
       .subscribe(data => {
         this.employee = data;
+        let config = new MatSnackBarConfig();
+    config.verticalPosition = this.verticalPosition;
+    config.horizontalPosition = this.horizontalPosition;
+    config.duration = 2000;
+      this.snackBar.open('Record saved successfully!', undefined, config);
+      this.employeeFormGroup.reset();
       }, error => console.log(error));
   }
 
   onSubmit() {
     this.save();
-    this.submitted = true;
   }
 
 }
