e6cb80e8f989ed10269d435653973624a542b278
Prafull Kulshrestha <prafullkulshrestha@ALIPL0857.local>
2020-04-09 17:23:23 +0530

    Ui layout changes

--- employer-portal/src/app/app.component.spec.ts
+++ employer-portal/src/app/app.component.spec.ts
@@ -4,11 +4,12 @@ import { AppComponent } from './app.component';
 import { EpSharedModule } from './shared/shared.module';
 import { By } from '@angular/platform-browser';
 import { MatToolbar, MatToolbarRow } from '../../node_modules/@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ RouterTestingModule, EpSharedModule ],
+      imports: [ RouterTestingModule, EpSharedModule, BrowserAnimationsModule ],
       declarations: [
         AppComponent
       ],
@@ -31,25 +32,12 @@ describe('AppComponent', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('mat-toolbar')).toBeTruthy();
   }));
-  it('should have mat-toolbar-row  tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('mat-toolbar-row')).toBeTruthy();
-  }));
-
-  it('should have anchor tags', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelectorAll('a').length).toEqual(3);
-  }));
-
+ 
   it('should have desired text contents for the first anchor tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect((compiled.querySelectorAll('a')[0]).textContent).toEqual('Enroll Employee');
+    expect((compiled.querySelectorAll('a')[0]).textContent).toEqual('About');
   }));
 
   it('should contain required texts', async(() => {
