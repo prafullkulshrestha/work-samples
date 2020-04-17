a1844767073c27f52da21477fb09c84741844b1a
Prafull Kulshrestha <prafullkulshrestha@ALIPL0857.local>
2020-04-17 10:03:09 +0530

    Service discovery client reading config from consul

--- config/consul/consu-keys.sh
+++ config/consul/consu-keys.sh
@@ -1,6 +1,10 @@
 # Global
 
-# For broker only
+consul kv put config/application/employer-api/instance/id employer-api
+consul kv put config/application/employer-api/context-path /employer/api
+consul kv put config/application/employer-api/departments/endpoint/uri /v1/departments
+
+# For employer only
 consul kv put config/employer-api/spring/datasource/driver-class-name org.postgresql.Driver
 consul kv put config/employer-api/spring/datasource/url jdbc:postgresql://postgres:5432/employer_api?currentSchema=employer
 consul kv put config/employer-api/spring/jpa/hibernate/show-sql true
